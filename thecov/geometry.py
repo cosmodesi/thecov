8fbc3cda69d8d58ae2cbeb887daa1943a2bcf910
Otávio Alves <oalves@protonmail.com>
2023-06-15 21:21:12 -0400

    Re-did mask calculations

--- thecov/geometry.py
+++ thecov/geometry.py
@@ -608,7 +608,6 @@ class SurveyGeometry(Geometry, base.FourierBinned):
             'dk': self.dk,
             'Nmesh': self.Nmesh,
             'delta_k_max': self.delta_k_max,
-            'kedges': self.kedges,
             'Nmodes': Nmodes,
         }
 
@@ -661,6 +660,317 @@ class SurveyGeometry(Geometry, base.FourierBinned):
 
     @staticmethod
     def _compute_window_kernel_row(args):
+
+        k1_bin_index, Bin_kmodes = args
+
+        I22 = shared_params['I22']
+
+        BoxSize = shared_params['BoxSize']
+        kfun = 2*np.pi/BoxSize,
+
+        nBins = shared_params['kbins']
+        kBinWidth = shared_params['dk']
+
+        Nmesh = shared_params['Nmesh']
+
+        W = shared_w
+
+        # Bin_ModeNum = utils.nmodes(BoxSize**3, kedges[:-1], kedges[1:])
+        Bin_ModeNum = shared_params['Nmodes']
+
+        # The Gaussian covariance drops quickly away from diagonal.
+        # Only delta_k_max points to each side of the diagonal are calculated.
+        delta_k_max = shared_params['delta_k_max']
+
+        icut = Nmesh//2
+        Nbin = k1_bin_index
+
+        avgWij = np.zeros((2*3+1,15,6))
+
+        avgW00 = np.zeros((2*3+1,15), dtype='<c8')
+        avgW22 = avgW00.copy()
+        avgW44 = avgW00.copy()
+        avgW20 = avgW00.copy()
+        avgW40 = avgW00.copy()
+        avgW42 = avgW00.copy()
+
+        ix,iy,iz,k2xh,k2yh,k2zh = np.zeros((6,2*icut+1, 2*icut+1, 2*icut+1))
+        
+        for i in range(2*icut+1):
+            ix[i,:,:] += i-icut
+            iy[:,i,:] += i-icut
+            iz[:,:,i] += i-icut
+
+
+        norm = len(Bin_kmodes)
+            
+        # if (kmodes_sampled < Bin_ModeNum[Nbin]):
+        #     norm = kmodes_sampled
+        #     sampled=(np.random.rand(kmodes_sampled)*Bin_ModeNum[Nbin]).astype(int)
+        # else:
+        #     norm = Bin_ModeNum[Nbin]
+        #     sampled=np.arange(Bin_ModeNum[Nbin],dtype=int)
+        
+        # Randomly select a mode in the k1 bin
+        for ik1x,ik1y,ik1z,rk1 in Bin_kmodes:
+            if (rk1 == 0.):
+                k1xh = 0
+                k1yh = 0
+                k1zh = 0
+            else:
+                k1xh = ik1x/rk1
+                k1yh = ik1y/rk1
+                k1zh = ik1z/rk1
+                
+            # Build a 3D array of modes around the selected mode   
+            k2xh = ik1x-ix
+            k2yh = ik1y-iy
+            k2zh = ik1z-iz
+
+            rk2 = np.sqrt(k2xh**2+k2yh**2+k2zh**2)
+            sort = (rk2*kfun/kBinWidth).astype(int)-Nbin # to decide later which shell the k2 mode belongs to
+            ind = (rk2 == 0)
+
+            if (ind.any() > 0):
+                rk2[ind] = 1e10
+
+            k2xh/=rk2
+            k2yh/=rk2
+            k2zh/=rk2
+            #k2 hat arrays built
+            
+            # Now calculating window multipole kernels by taking dot products of cartesian FFTs with k1-hat, k2-hat arrays
+            # W corresponds to W22(k) and Wc corresponds to conjugate of W22(k)
+            # L(i) refers to multipoles
+                
+            # k1xh is a scalar
+            # k2xh.shape = (31,31,31)
+            
+            W_L0 = W['22']
+            Wc_L0 = np.conj(W['22'])
+
+            xx=W['22xx']*k1xh**2+W['22yy']*k1yh**2+W['22zz']*k1zh**2+2.*W['22xy']*k1xh*k1yh+2.*W['22yz']*k1yh*k1zh+2.*W['22xz']*k1zh*k1xh
+            
+            W_k1L2=1.5*xx-0.5*W['22']
+            W_k2L2=1.5*(W['22xx']*k2xh**2+W['22yy']*k2yh**2+W['22zz']*k2zh**2 \
+            +2.*W['22xy']*k2xh*k2yh+2.*W['22yz']*k2yh*k2zh+2.*W['22xz']*k2zh*k2xh)-0.5*W['22']
+            Wc_k1L2=np.conj(W_k1L2)
+            Wc_k2L2=np.conj(W_k2L2)
+            
+            W_k1L4=35./8.*(W['22xxxx']*k1xh**4 +W['22yyyy']*k1yh**4+W['22zzzz']*k1zh**4 \
+        +4.*W['22xxxy']*k1xh**3*k1yh +4.*W['22xxxz']*k1xh**3*k1zh +4.*W['22xyyy']*k1yh**3*k1xh \
+        +4.*W['22yyyz']*k1yh**3*k1zh +4.*W['22xzzz']*k1zh**3*k1xh +4.*W['22yzzz']*k1zh**3*k1yh \
+        +6.*W['22xxyy']*k1xh**2*k1yh**2+6.*W['22xxzz']*k1xh**2*k1zh**2+6.*W['22yyzz']*k1yh**2*k1zh**2 \
+        +12.*W['22xxyz']*k1xh**2*k1yh*k1zh+12.*W['22xyyz']*k1yh**2*k1xh*k1zh +12.*W['22xyzz']*k1zh**2*k1xh*k1yh) \
+        -5./2.*W_k1L2 -7./8.*W_L0
+            Wc_k1L4=np.conj(W_k1L4)
+            
+            k1k2=W['22xxxx']*(k1xh*k2xh)**2+W['22yyyy']*(k1yh*k2yh)**2+W['22zzzz']*(k1zh*k2zh)**2 \
+                +W['22xxxy']*(k1xh*k1yh*k2xh**2+k1xh**2*k2xh*k2yh)*2\
+                +W['22xxxz']*(k1xh*k1zh*k2xh**2+k1xh**2*k2xh*k2zh)*2\
+                +W['22yyyz']*(k1yh*k1zh*k2yh**2+k1yh**2*k2yh*k2zh)*2\
+                +W['22yzzz']*(k1zh*k1yh*k2zh**2+k1zh**2*k2zh*k2yh)*2\
+                +W['22xyyy']*(k1yh*k1xh*k2yh**2+k1yh**2*k2yh*k2xh)*2\
+                +W['22xzzz']*(k1zh*k1xh*k2zh**2+k1zh**2*k2zh*k2xh)*2\
+                +W['22xxyy']*(k1xh**2*k2yh**2+k1yh**2*k2xh**2+4.*k1xh*k1yh*k2xh*k2yh)\
+                +W['22xxzz']*(k1xh**2*k2zh**2+k1zh**2*k2xh**2+4.*k1xh*k1zh*k2xh*k2zh)\
+                +W['22yyzz']*(k1yh**2*k2zh**2+k1zh**2*k2yh**2+4.*k1yh*k1zh*k2yh*k2zh)\
+                +W['22xyyz']*(k1xh*k1zh*k2yh**2+k1yh**2*k2xh*k2zh+2.*k1yh*k2yh*(k1zh*k2xh+k1xh*k2zh))*2\
+                +W['22xxyz']*(k1yh*k1zh*k2xh**2+k1xh**2*k2yh*k2zh+2.*k1xh*k2xh*(k1zh*k2yh+k1yh*k2zh))*2\
+                +W['22xyzz']*(k1yh*k1xh*k2zh**2+k1zh**2*k2yh*k2xh+2.*k1zh*k2zh*(k1xh*k2yh+k1yh*k2xh))*2
+            
+            W_k2L4=35./8.*(W['22xxxx']*k2xh**4 +W['22yyyy']*k2yh**4+W['22zzzz']*k2zh**4 \
+        +4.*W['22xxxy']*k2xh**3*k2yh +4.*W['22xxxz']*k2xh**3*k2zh +4.*W['22xyyy']*k2yh**3*k2xh \
+        +4.*W['22yyyz']*k2yh**3*k2zh +4.*W['22xzzz']*k2zh**3*k2xh +4.*W['22yzzz']*k2zh**3*k2yh \
+        +6.*W['22xxyy']*k2xh**2*k2yh**2+6.*W['22xxzz']*k2xh**2*k2zh**2+6.*W['22yyzz']*k2yh**2*k2zh**2 \
+        +12.*W['22xxyz']*k2xh**2*k2yh*k2zh+12.*W['22xyyz']*k2yh**2*k2xh*k2zh +12.*W['22xyzz']*k2zh**2*k2xh*k2yh) \
+        -5./2.*W_k2L2 -7./8.*W_L0
+            Wc_k2L4=np.conj(W_k2L4)
+            
+            W_k1L2_k2L2= 9./4.*k1k2 -3./4.*xx -1./2.*W_k2L2
+            W_k1L2_k2L4=2/7.*W_k1L2+20/77.*W_k1L4 #approximate as 6th order FFTs not simulated
+            W_k1L4_k2L2=W_k1L2_k2L4 #approximate
+            W_k1L4_k2L4=1/9.*W_L0+100/693.*W_k1L2+162/1001.*W_k1L4
+            Wc_k1L2_k2L2= np.conj(W_k1L2_k2L2)
+            Wc_k1L2_k2L4=np.conj(W_k1L2_k2L4)
+            Wc_k1L4_k2L2=Wc_k1L2_k2L4
+            Wc_k1L4_k2L4=np.conj(W_k1L4_k2L4)
+            
+            k1k2W12=np.conj(W['12xxxx'])*(k1xh*k2xh)**2+np.conj(W['12yyyy'])*(k1yh*k2yh)**2+np.conj(W['12zzzz'])*(k1zh*k2zh)**2 \
+                +np.conj(W['12xxxy'])*(k1xh*k1yh*k2xh**2+k1xh**2*k2xh*k2yh)*2\
+                +np.conj(W['12xxxz'])*(k1xh*k1zh*k2xh**2+k1xh**2*k2xh*k2zh)*2\
+                +np.conj(W['12yyyz'])*(k1yh*k1zh*k2yh**2+k1yh**2*k2yh*k2zh)*2\
+                +np.conj(W['12yzzz'])*(k1zh*k1yh*k2zh**2+k1zh**2*k2zh*k2yh)*2\
+                +np.conj(W['12xyyy'])*(k1yh*k1xh*k2yh**2+k1yh**2*k2yh*k2xh)*2\
+                +np.conj(W['12xzzz'])*(k1zh*k1xh*k2zh**2+k1zh**2*k2zh*k2xh)*2\
+                +np.conj(W['12xxyy'])*(k1xh**2*k2yh**2+k1yh**2*k2xh**2+4.*k1xh*k1yh*k2xh*k2yh)\
+                +np.conj(W['12xxzz'])*(k1xh**2*k2zh**2+k1zh**2*k2xh**2+4.*k1xh*k1zh*k2xh*k2zh)\
+                +np.conj(W['12yyzz'])*(k1yh**2*k2zh**2+k1zh**2*k2yh**2+4.*k1yh*k1zh*k2yh*k2zh)\
+                +np.conj(W['12xyyz'])*(k1xh*k1zh*k2yh**2+k1yh**2*k2xh*k2zh+2.*k1yh*k2yh*(k1zh*k2xh+k1xh*k2zh))*2\
+                +np.conj(W['12xxyz'])*(k1yh*k1zh*k2xh**2+k1xh**2*k2yh*k2zh+2.*k1xh*k2xh*(k1zh*k2yh+k1yh*k2zh))*2\
+                +np.conj(W['12xyzz'])*(k1yh*k1xh*k2zh**2+k1zh**2*k2yh*k2xh+2.*k1zh*k2zh*(k1xh*k2yh+k1yh*k2xh))*2
+            
+            xxW12=np.conj(W['12xx'])*k1xh**2+np.conj(W['12yy'])*k1yh**2+np.conj(W['12zz'])*k1zh**2+2.*np.conj(W['12xy'])*k1xh*k1yh+2.*np.conj(W['12yz'])*k1yh*k1zh+2.*np.conj(W['12xz'])*k1zh*k1xh
+        
+            W12_L0 = np.conj(W['12'])
+            W12_k1L2=1.5*xxW12-0.5*np.conj(W['12'])
+            W12_k1L4=35./8.*(np.conj(W['12xxxx'])*k1xh**4 +np.conj(W['12yyyy'])*k1yh**4+np.conj(W['12zzzz'])*k1zh**4 \
+        +4.*np.conj(W['12xxxy'])*k1xh**3*k1yh +4.*np.conj(W['12xxxz'])*k1xh**3*k1zh +4.*np.conj(W['12xyyy'])*k1yh**3*k1xh \
+        +6.*np.conj(W['12xxyy'])*k1xh**2*k1yh**2+6.*np.conj(W['12xxzz'])*k1xh**2*k1zh**2+6.*np.conj(W['12yyzz'])*k1yh**2*k1zh**2 \
+        +12.*np.conj(W['12xxyz'])*k1xh**2*k1yh*k1zh+12.*np.conj(W['12xyyz'])*k1yh**2*k1xh*k1zh +12.*np.conj(W['12xyzz'])*k1zh**2*k1xh*k1yh) \
+        -5./2.*W12_k1L2 -7./8.*W12_L0
+            W12_k1L4_k2L2=2/7.*W12_k1L2+20/77.*W12_k1L4
+            W12_k1L4_k2L4=1/9.*W12_L0+100/693.*W12_k1L2+162/1001.*W12_k1L4
+            W12_k2L2=1.5*(np.conj(W['12xx'])*k2xh**2+np.conj(W['12yy'])*k2yh**2+np.conj(W['12zz'])*k2zh**2\
+            +2.*np.conj(W['12xy'])*k2xh*k2yh+2.*np.conj(W['12yz'])*k2yh*k2zh+2.*np.conj(W['12xz'])*k2zh*k2xh)-0.5*np.conj(W['12'])
+            W12_k2L4=35./8.*(np.conj(W['12xxxx'])*k2xh**4 +np.conj(W['12yyyy'])*k2yh**4+np.conj(W['12zzzz'])*k2zh**4 \
+        +4.*np.conj(W['12xxxy'])*k2xh**3*k2yh +4.*np.conj(W['12xxxz'])*k2xh**3*k2zh +4.*np.conj(W['12xyyy'])*k2yh**3*k2xh \
+        +4.*np.conj(W['12yyyz'])*k2yh**3*k2zh +4.*np.conj(W['12xzzz'])*k2zh**3*k2xh +4.*np.conj(W['12yzzz'])*k2zh**3*k2yh \
+        +6.*np.conj(W['12xxyy'])*k2xh**2*k2yh**2+6.*np.conj(W['12xxzz'])*k2xh**2*k2zh**2+6.*np.conj(W['12yyzz'])*k2yh**2*k2zh**2 \
+        +12.*np.conj(W['12xxyz'])*k2xh**2*k2yh*k2zh+12.*np.conj(W['12xyyz'])*k2yh**2*k2xh*k2zh +12.*np.conj(W['12xyzz'])*k2zh**2*k2xh*k2yh) \
+        -5./2.*W12_k2L2 -7./8.*W12_L0
+            
+            W12_k1L2_k2L2= 9./4.*k1k2W12 -3./4.*xxW12 -1./2.*W12_k2L2
+            
+            W_k1L2_Sumk2L22=1/5.*W_k1L2+2/7.*W_k1L2_k2L2+18/35.*W_k1L2_k2L4
+            W_k1L2_Sumk2L24=2/7.*W_k1L2_k2L2+20/77.*W_k1L2_k2L4
+            W_k1L4_Sumk2L22=1/5.*W_k1L4+2/7.*W_k1L4_k2L2+18/35.*W_k1L4_k2L4
+            W_k1L4_Sumk2L24=2/7.*W_k1L4_k2L2+20/77.*W_k1L4_k2L4
+            W_k1L4_Sumk2L44=1/9.*W_k1L4+100/693.*W_k1L4_k2L2+162/1001.*W_k1L4_k2L4
+            
+            C00exp = [Wc_L0*W_L0,Wc_L0*W_k2L2,Wc_L0*W_k2L4,\
+                    Wc_k1L2*W_L0,Wc_k1L2*W_k2L2,Wc_k1L2*W_k2L4,\
+                    Wc_k1L4*W_L0,Wc_k1L4*W_k2L2,Wc_k1L4*W_k2L4]
+            
+            C00exp += [2.*W_L0*W12_L0,W_k1L2*W12_L0,W_k1L4*W12_L0,\
+                    W_k2L2*W12_L0,W_k2L4*W12_L0,np.conj(W12_L0)*W12_L0]
+            
+            C22exp = [Wc_k2L2*W_k1L2 + Wc_L0*W_k1L2_k2L2,\
+                    Wc_k2L2*W_k1L2_k2L2 + Wc_L0*W_k1L2_Sumk2L22,\
+                    Wc_k2L2*W_k1L2_k2L4 + Wc_L0*W_k1L2_Sumk2L24,\
+                    Wc_k1L2_k2L2*W_k1L2 + Wc_k1L2*W_k1L2_k2L2,\
+                    Wc_k1L2_k2L2*W_k1L2_k2L2 + Wc_k1L2*W_k1L2_Sumk2L22,\
+                    Wc_k1L2_k2L2*W_k1L2_k2L4 + Wc_k1L2*W_k1L2_Sumk2L24,\
+                    Wc_k1L4_k2L2*W_k1L2 + Wc_k1L4*W_k1L2_k2L2,\
+                    Wc_k1L4_k2L2*W_k1L2_k2L2 + Wc_k1L4*W_k1L2_Sumk2L22,\
+                    Wc_k1L4_k2L2*W_k1L2_k2L4 + Wc_k1L4*W_k1L2_Sumk2L24]
+            
+            C22exp += [W_k1L2*W12_k2L2 + W_k2L2*W12_k1L2\
+                    +W_k1L2_k2L2*W12_L0+W_L0*W12_k1L2_k2L2,\
+                    0.5*((1/5.*W_L0+2/7.*W_k1L2+18/35.*W_k1L4)*W12_k2L2 + W_k1L2_k2L2*W12_k1L2\
+    +(1/5.*W_k2L2+2/7.*W_k1L2_k2L2+18/35.*W_k1L4_k2L2)*W12_L0 + W_k1L2*W12_k1L2_k2L2),\
+        0.5*((2/7.*W_k1L2+20/77.*W_k1L4)*W12_k2L2 + W_k1L4_k2L2*W12_k1L2\
+    +(2/7.*W_k1L2_k2L2+20/77.*W_k1L4_k2L2)*W12_L0 + W_k1L4*W12_k1L2_k2L2),\
+    0.5*(W_k1L2_k2L2*W12_k2L2+(1/5.*W_L0+2/7.*W_k2L2+18/35.*W_k2L4)*W12_k1L2\
+    +(1/5.*W_k1L2+2/7.*W_k1L2_k2L2+18/35.*W_k1L2_k2L4)*W12_L0 + W_k2L2*W12_k1L2_k2L2),\
+    0.5*(W_k1L2_k2L4*W12_k2L2+(2/7.*W_k2L2+20/77.*W_k2L4)*W12_k1L2\
+    +(2/7.*W_k1L2_k2L2+20/77.*W_k1L2_k2L4)*W12_L0 + W_k2L4*W12_k1L2_k2L2),\
+                    np.conj(W12_k1L2_k2L2)*W12_L0+np.conj(W12_k1L2)*W12_k2L2]
+            
+            C44exp = [Wc_k2L4*W_k1L4 + Wc_L0*W_k1L4_k2L4,\
+                    Wc_k2L4*W_k1L4_k2L2 + Wc_L0*W_k1L4_Sumk2L24,\
+                    Wc_k2L4*W_k1L4_k2L4 + Wc_L0*W_k1L4_Sumk2L44,\
+                    Wc_k1L2_k2L4*W_k1L4 + Wc_k1L2*W_k1L4_k2L4,\
+                    Wc_k1L2_k2L4*W_k1L4_k2L2 + Wc_k1L2*W_k1L4_Sumk2L24,\
+                    Wc_k1L2_k2L4*W_k1L4_k2L4 + Wc_k1L2*W_k1L4_Sumk2L44,\
+                    Wc_k1L4_k2L4*W_k1L4 + Wc_k1L4*W_k1L4_k2L4,\
+                    Wc_k1L4_k2L4*W_k1L4_k2L2 + Wc_k1L4*W_k1L4_Sumk2L24,\
+                    Wc_k1L4_k2L4*W_k1L4_k2L4 + Wc_k1L4*W_k1L4_Sumk2L44]
+            
+            C44exp += [W_k1L4*W12_k2L4 + W_k2L4*W12_k1L4\
+                    +W_k1L4_k2L4*W12_L0+W_L0*W12_k1L4_k2L4,\
+                    0.5*((2/7.*W_k1L2+20/77.*W_k1L4)*W12_k2L4 + W_k1L2_k2L4*W12_k1L4\
+    +(2/7.*W_k1L2_k2L4+20/77.*W_k1L4_k2L4)*W12_L0 + W_k1L2*W12_k1L4_k2L4),\
+    0.5*((1/9.*W_L0+100/693.*W_k1L2+162/1001.*W_k1L4)*W12_k2L4 + W_k1L4_k2L4*W12_k1L4\
+    +(1/9.*W_k2L4+100/693.*W_k1L2_k2L4+162/1001.*W_k1L4_k2L4)*W12_L0 + W_k1L4*W12_k1L4_k2L4),\
+    0.5*(W_k1L4_k2L2*W12_k2L4+(2/7.*W_k2L2+20/77.*W_k2L4)*W12_k1L4\
+    +(2/7.*W_k1L4_k2L2+20/77.*W_k1L4_k2L4)*W12_L0 + W_k2L2*W12_k1L4_k2L4),\
+    0.5*(W_k1L4_k2L4*W12_k2L4+(1/9.*W_L0+100/693.*W_k2L2+162/1001.*W_k2L4)*W12_k1L4\
+    +(1/9.*W_k1L4+100/693.*W_k1L4_k2L2+162/1001.*W_k1L4_k2L4)*W12_L0 + W_k2L4*W12_k1L4_k2L4),\
+                    np.conj(W12_k1L4_k2L4)*W12_L0+np.conj(W12_k1L4)*W12_k2L4] #1/(nbar)^2
+            
+            C20exp = [Wc_L0*W_k1L2,Wc_L0*W_k1L2_k2L2,Wc_L0*W_k1L2_k2L4,\
+                    Wc_k1L2*W_k1L2,Wc_k1L2*W_k1L2_k2L2,Wc_k1L2*W_k1L2_k2L4,\
+                    Wc_k1L4*W_k1L2,Wc_k1L4*W_k1L2_k2L2,Wc_k1L4*W_k1L2_k2L4]
+            
+            C20exp += [W_k1L2*W12_L0 + W_L0*W12_k1L2,\
+                    0.5*((1/5.*W_L0+2/7.*W_k1L2+18/35.*W_k1L4)*W12_L0 + W_k1L2*W12_k1L2),\
+                    0.5*((2/7.*W_k1L2+20/77.*W_k1L4)*W12_L0 + W_k1L4*W12_k1L2),\
+                    0.5*(W_k1L2_k2L2*W12_L0 + W_k2L2*W12_k1L2),\
+                    0.5*(W_k1L2_k2L4*W12_L0 + W_k2L4*W12_k1L2),\
+                    np.conj(W12_k1L2)*W12_L0]
+            
+            C40exp = [Wc_L0*W_k1L4,Wc_L0*W_k1L4_k2L2,Wc_L0*W_k1L4_k2L4,\
+                    Wc_k1L2*W_k1L4,Wc_k1L2*W_k1L4_k2L2,Wc_k1L2*W_k1L4_k2L4,\
+                    Wc_k1L4*W_k1L4,Wc_k1L4*W_k1L4_k2L2,Wc_k1L4*W_k1L4_k2L4]
+            
+            C40exp += [W_k1L4*W12_L0 + W_L0*W12_k1L4,\
+                    0.5*((2/7.*W_k1L2+20/77.*W_k1L4)*W12_L0 + W_k1L2*W12_k1L4),\
+                    0.5*((1/9.*W['22']+100/693.*W_k1L2+162/1001.*W_k1L4)*W12_L0 + W_k1L4*W12_k1L4),\
+                    0.5*(W_k1L4_k2L2*W12_L0 + W_k2L2*W12_k1L4),\
+                    0.5*(W_k1L4_k2L4*W12_L0 + W_k2L4*W12_k1L4),\
+                    np.conj(W12_k1L4)*W12_L0]
+            
+            C42exp = [Wc_k2L2*W_k1L4 + Wc_L0*W_k1L4_k2L2,\
+                    Wc_k2L2*W_k1L4_k2L2 + Wc_L0*W_k1L4_Sumk2L22,\
+                    Wc_k2L2*W_k1L4_k2L4 + Wc_L0*W_k1L4_Sumk2L24,\
+                    Wc_k1L2_k2L2*W_k1L4 + Wc_k1L2*W_k1L4_k2L2,\
+                    Wc_k1L2_k2L2*W_k1L4_k2L2 + Wc_k1L2*W_k1L4_Sumk2L22,\
+                    Wc_k1L2_k2L2*W_k1L4_k2L4 + Wc_k1L2*W_k1L4_Sumk2L24,\
+                    Wc_k1L4_k2L2*W_k1L4 + Wc_k1L4*W_k1L4_k2L2,\
+                    Wc_k1L4_k2L2*W_k1L4_k2L2 + Wc_k1L4*W_k1L4_Sumk2L22,\
+                    Wc_k1L4_k2L2*W_k1L4_k2L4 + Wc_k1L4*W_k1L4_Sumk2L24]
+            
+            C42exp += [W_k1L4*W12_k2L2 + W_k2L2*W12_k1L4+\
+                    W_k1L4_k2L2*W12_L0+W['22']*W12_k1L4_k2L2,\
+                    0.5*((2/7.*W_k1L2+20/77.*W_k1L4)*W12_k2L2 + W_k1L2_k2L2*W12_k1L4\
+        +(2/7.*W_k1L2_k2L2+20/77.*W_k1L4_k2L2)*W12_L0 + W_k1L2*W12_k1L4_k2L2),\
+        0.5*((1/9.*W['22']+100/693.*W_k1L2+162/1001.*W_k1L4)*W12_k2L2 + W_k1L4_k2L2*W12_k1L4\
+    +(1/9.*W_k2L2+100/693.*W_k1L2_k2L2+162/1001.*W_k1L4_k2L2)*W12_L0 + W_k1L4*W12_k1L4_k2L2),\
+    0.5*(W_k1L4_k2L2*W12_k2L2+(1/5.*W['22']+2/7.*W_k2L2+18/35.*W_k2L4)*W12_k1L4\
+    +(1/5.*W_k1L4+2/7.*W_k1L4_k2L2+18/35.*W_k1L4_k2L4)*W12_L0 + W_k2L2*W12_k1L4_k2L2),\
+    0.5*(W_k1L4_k2L4*W12_k2L2+(2/7.*W_k2L2+20/77.*W_k2L4)*W12_k1L4\
+    +(2/7.*W_k1L4_k2L2+20/77.*W_k1L4_k2L4)*W12_L0 + W_k2L4*W12_k1L4_k2L2),\
+                    np.conj(W12_k1L4_k2L2)*W12_L0+np.conj(W12_k1L4)*W12_k2L2] #1/(nbar)^2
+            
+            for i in range(-3,4):
+                ind = (sort == i)
+                for j in range(15):
+                    avgW00[i+3,j] += np.sum(C00exp[j][ind])
+                    avgW22[i+3,j] += np.sum(C22exp[j][ind])
+                    avgW44[i+3,j] += np.sum(C44exp[j][ind])
+                    avgW20[i+3,j] += np.sum(C20exp[j][ind])
+                    avgW40[i+3,j] += np.sum(C40exp[j][ind])
+                    avgW42[i+3,j] += np.sum(C42exp[j][ind])
+                
+        for i in range(0,2*3+1):
+            if(i+Nbin-3>=nBins or i+Nbin-3<0): 
+                avgW00[i] *= 0
+                avgW22[i] *= 0
+                avgW44[i] *= 0
+                avgW20[i] *= 0
+                avgW40[i] *= 0
+                avgW42[i] *= 0
+                continue
+
+            avgW00[i] = avgW00[i]/(norm*Bin_ModeNum[Nbin+i-3]*I22**2)
+            avgW22[i] = avgW22[i]/(norm*Bin_ModeNum[Nbin+i-3]*I22**2)
+            avgW44[i] = avgW44[i]/(norm*Bin_ModeNum[Nbin+i-3]*I22**2)
+            avgW20[i] = avgW20[i]/(norm*Bin_ModeNum[Nbin+i-3]*I22**2)
+            avgW40[i] = avgW40[i]/(norm*Bin_ModeNum[Nbin+i-3]*I22**2)
+            avgW42[i] = avgW42[i]/(norm*Bin_ModeNum[Nbin+i-3]*I22**2)
+            
+        avgWij[:,:,0] =    2.*np.real(avgW00)
+        avgWij[:,:,1] =   25.*np.real(avgW22)
+        avgWij[:,:,2] =   81.*np.real(avgW44)
+        avgWij[:,:,3] = 5.*2.*np.real(avgW20)
+        avgWij[:,:,4] = 9.*2.*np.real(avgW40)
+        avgWij[:,:,5] =   45.*np.real(avgW42)
+        
+        return avgWij
+
+    @staticmethod
+    def _compute_window_kernel_row_original(args):
         '''Computes a row of the window kernels. This function is called in parallel for each k1 bin.'''
         # Gives window kernels for L=0,2,4 auto and cross covariance (instead of only L=0 above)
 
