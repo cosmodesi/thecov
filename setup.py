3183f16245773f7478a97afdf14853ce919c3a9f
Otávio Alves <alonso.otavio@gmail.com>
2023-09-21 12:32:11 -0400

    Declared version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thecov',
-    version='0.1.2',    
+    version='0.1.4',    
     description='A python package to compute analytic covariance matrices based on Wadekar & Scoccimarro 2019 (arXiv:1910.02914).',
     url='https://github.com/cosmodesi/thecov',
     author='Otavio Alves',
